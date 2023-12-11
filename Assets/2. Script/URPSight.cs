ebb2233d561196c187f8b510152a4ee30d7a044a
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-12-12 00:07:46 +0900

    Add comments

--- Assets/2. Script/URPSight.cs
+++ Assets/2. Script/URPSight.cs
@@ -5,26 +5,45 @@ public class URPSight : MonoBehaviour
     
     #region Variables
 
+    [Header("Transform components for raycasting")]
     private Transform cachedTransform = null;
     private Transform sightObjectTransform = null;
 
+    [Header("Components for instantiating the sight mesh")]
     private Mesh sightMesh = null;
     private MeshFilter sightMeshFilter = null;
 
+    /// <summary>
+    /// Field of view value.
+    /// </summary>
     private float fov = 90f;
 
+    /// <summary>
+    /// The variable determining how much to check for raycasting.
+    /// In other words, it determines at what angle intervals raycasting will be checked.
+    /// If the value is 360, which is default value, it will check raycasting every 1 degree.
+    /// </summary>
     private int rayCount = 360;
 
+    /// <summary>
+    /// The max distance of raycasting within the FOV range.
+    /// </summary>
     private float viewDistance = 8f;
+    
+    /// <summary>
+    /// The max distance of raycasting outside the FOV range.
+    /// </summary>
     private float surroundingDistance = 2f;
 
+    /// <summary>
+    /// The layer mask for walls.
+    /// The sight does not reach behind walls.
+    /// </summary>
     private LayerMask layerMask;
 
     #endregion Variables
 
-    #region URP Sight
-
-    #endregion URP Sight
+    #region Unity Events
 
     private void Awake()
     {
@@ -44,6 +63,15 @@ public class URPSight : MonoBehaviour
         DrawSightWithURP(Vector2.right);
     }
 
+    #endregion Unity Events
+
+    /// <summary>
+    /// Generate a sight mesh based on the given direction.
+    /// The mesh is formed by connecting triangles to approximate a circle.
+    /// The interval of the angle at which raycasting is checked changes according to the rayCount value.
+    /// As the rayCount increases, the mesh becomes closer to a circle, but the computational cost also increases.
+    /// </summary>
+    /// <param name="direction">The direction the character is facing</param>
     private void DrawSightWithURP(Vector2 direction)
     {
         Vector3 position = cachedTransform.position;
@@ -53,12 +81,15 @@ public class URPSight : MonoBehaviour
         startAngle = angle = Utilities.GetAngleFromVector(direction) - (fov * 0.5f);
         endAngle = startAngle + fov;
         
+        // Calculate the interval of the angle
         float angleIncrease = 360f / rayCount;
 
+        // Initialize the arrays to construct triangles
         Vector3[] vertices = new Vector3[rayCount + 1 + 1];
         Vector2[] uv = new Vector2[vertices.Length];
         int[] triangles = new int[rayCount * 3];
 
+        // Set the origin of the FOV as the first vertex
         vertices[0] = origin - position;
 
         int vertexIndex = 1;
@@ -70,6 +101,7 @@ public class URPSight : MonoBehaviour
             float viewDistance = angle < startAngle || angle > endAngle ? surroundingDistance : this.viewDistance;
             RaycastHit2D hitObject = Physics2D.Raycast(origin, rayDirection, viewDistance, layerMask);
 
+            // if there is a wall within the range
             if (hitObject)
             {
                 vertex = hitObject.point;
@@ -82,12 +114,12 @@ public class URPSight : MonoBehaviour
             vertex -= position;
             vertices[vertexIndex] = vertex;
 
+            // Set the vertex to form the triangle
             if (index > 0)
             {
-                triangles[triangleIndex] = 0;
-                triangles[triangleIndex + 1] = vertexIndex - 1;
-                triangles[triangleIndex] = vertexIndex;
-                triangleIndex += 3;
+                triangles[triangleIndex++] = 0;
+                triangles[triangleIndex++] = vertexIndex - 1;
+                triangles[triangleIndex++] = vertexIndex;
             }
 
             vertexIndex++;
